78df66f68d7addf954febafe87143cd1fdb20bf8
Ivani <katarzyna.stosio@gmail.com>
2020-02-09 10:08:22 +0100

    100% performance

--- tapeEquilibrium.js
+++ tapeEquilibrium.js
@@ -1,28 +1,44 @@
 
-function sum(arr, start){
-    /*return arr.reduce(function(sum, currentValue) {
-            return sum + currentValue;  
-        }, start);*/
-    let sum = start;
-    for(let i = 0; i < arr.length; i++){
-        sum += arr[i];
-    }
-    return sum;
-}
 
+/*
 function solution(A) {
     let difference = 0;
     let firstPart = 0;
     let secondPart = 0;
-    const max = A.length - 1;
-    let res = Number.MAX_SAFE_INTEGER;
+    const length = A.length;
+    const max = length - 1;
+    let res = 100000000;
     for(let i = 0; i < max; i++){
-        firstPart = sum([...A].splice(0, i + 1), 0);
-        secondPart = sum([...A].splice(i + 1, max - i), 0); 
-        difference = Math.abs(firstPart - secondPart);
+        const val = i + 1;
+        for(let j = 0; j < length; j++){
+            if(j < val){
+                firstPart += A[j];
+            } else {
+                secondPArt += A[j];
+            }
+        }
+        const diff = firstPart - secondPart;
+        difference = diff < 0 ? diff * -1 : diff;
         if(difference < res){
             res = difference;
         }
     }
     return res;
 }
+*/
+
+
+function solution(A) {
+    let leftSum = 0; 
+    let rightSum = 0;
+    let arr = []; 
+    const sum = A.reduce((a, b) => a + b);
+    const length = A.length;
+    for (let i = 1; i < length; i++) {
+      leftSum += A[i-1]; 
+      rightSum = sum - leftSum; 
+      arr.push(Math.abs(rightSum - leftSum));
+    } 
+      return Math.min(...arr);
+  }
+  solution([-10, -20, -30, -40, 100]);
