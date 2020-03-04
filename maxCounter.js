e2fcfec7afb44037c2087233a544f02cf2b3d51c
ivanicode <47351901+ivanicode@users.noreply.github.com>
2020-03-04 18:03:25 +0100

    Update maxCounter.js

--- maxCounter.js
+++ maxCounter.js
@@ -1,25 +1,25 @@
 function solution(N, A) {
     let countersArray = Array(N).fill(0); //[0, 0, 0, 0 ,0]
-    let biggestCounter = 0;
-    let lastFillValue = 0;
+    let biggestCounter = 0; //najwieksza liczba w tablicy
+    let lastFillValue = 0; //ostatnia liczba, ktora zastapila wszystkie liczby w tablicy
     for(let i = 0; i < A.length; i++){
-        if(A[i] > N){
+        if(A[i] > N){ //jezeli wartosc elementu w tablicy jest wieksza od N..
             //countersArray.fill(biggestCounter);
-            lastFillValue = biggestCounter;
-        } else {
-            const index = A[i] - 1;
-            if(countersArray[index] < lastFillValue){
-                countersArray[index] = lastFillValue;
+            lastFillValue = biggestCounter; //wypelniamy wszystkie elementy w tablicy najwieksza liczba
+        } else { //jezeli wartosc elementu w tablicy jest mniejsza od N...
+            const index = A[i] - 1; //tworzymy zmienna ktorej wartosc bedzie rowna indexowi elementu na ktorym pracujemy
+            if(countersArray[index] < lastFillValue){ //warunek: jezeli element w tablicy jest mniejszy od ostatniej najwiekszej wartosci jaka wypelnilismy tablice
+                countersArray[index] = lastFillValue; //to nadpisujemy ja ta najwieksza wartoscia
             }
-            countersArray[index]++;
-            if(countersArray[index] > biggestCounter){
-                biggestCounter = countersArray[index];
+            countersArray[index]++; //wykonujemy inkrementacje na kazdym elemencje tablicy po kolei
+            if(countersArray[index] > biggestCounter){ //warunek: jezeli kazdy element tablicy jest wiekszy od najwiekszej poprzednio wartosci w tablicy
+                biggestCounter = countersArray[index]; //to najwieksza wartosc w tablicy zostaje uaktualniona
             }
         }
     }
     for(let j = 0; j < N; j++){
-        if(countersArray[j] < lastFillValue){
-            countersArray[j] = lastFillValue;
+        if(countersArray[j] < lastFillValue){ //warunek: jezeli jakis element w tablicy jest mniejszy od ostatniej najwiekszej liczby, ktora nadpisala wszystkie liczby w tablicy
+            countersArray[j] = lastFillValue; //to tady element tablicy zostaje nadpisany przez ta liczbe
         }
     }
     return countersArray;
