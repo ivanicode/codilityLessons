f3afedfa646dced06919f356087406f2e4957df7
ivanicode <47351901+ivanicode@users.noreply.github.com>
2020-07-23 17:00:51 +0200

    Update cyclicRotation.js

--- cyclicRotation.js
+++ cyclicRotation.js
@@ -32,3 +32,10 @@ function solution1(arr, K){
     return newArr;
 }
 console.log(solution([1, 2, 3, 4, 5], 2));
+
+function solution(A, K) {
+    const modulo = K % A.length; //Tutaj obliczana jest reszta z dzielenia zmiennej K przez długość tablicy
+    const sliceIndex = A.length - modulo; //Tutaj do zmiennej sliceIndex przypisuje się index tablicy, który będzie dzielił tablicę na dwie części
+    return [...A.slice(sliceIndex), ...A.slice(0, sliceIndex)] //Tutaj dzięki wcześniejszym obliczeniom tworzymy tablicę złożoną z dwóch części - pierwsza: wcześniejsza druga - i druga - wcześniejsza pierwsza. W ten sposób posługując się metodą wbudowaną slice() w prosty sposób zwracamy tablicę z elementami pod właściwym indexem.
+}
+
