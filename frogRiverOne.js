3f9bdf8a8447efb743b11d97bffe0b2e5bdd8da9
ivanicode <47351901+ivanicode@users.noreply.github.com>
2020-08-18 14:53:39 +0200

    Update frogRiverOne.js
    New shorter solution with use new Set

--- frogRiverOne.js
+++ frogRiverOne.js
@@ -27,3 +27,16 @@ function solution(X, A) {
   }
   return -1;
 }
+
+function solution(X, A) {
+    let set = new Set(); //Tworzymy obiekt Set, który gwarantuje nam niepowtarzalność elementów
+    for(let i = 0; i < A.length; i++){ //Tworzymy pętlę dla tablicy A
+        if(A[i] <= X){ //W tym warunku wrzucamy do Seta wszystkie liczby mniejsze bądź równe wartości X
+            set.add(A[i]);
+        }
+        if(set.size === X){ //W tym warunku gdy ilość liczb w Secie jest taka sama co wartość X i mamy pewność, że wszystkie pozycje od 1 do X są, zwracamy index liczby równej wartości X, czyli sekunde, w której żabka przedostaje się na drugi brzeg.
+            return i;
+        }
+    }
+return -1; //Jeżeli żabka nie ma możliwości przeskoczyć na drugi brzeg mamy zwrócić -1
+}
